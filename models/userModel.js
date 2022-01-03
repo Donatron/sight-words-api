487382004ffab12af79927c05cc03adf2acc1034
Donatron <don.macarthur68@gmail.com>
2022-01-03 13:54:52 +0700

    password-reset: added password reset and forgot password funtionality and password reset token email

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -75,6 +76,15 @@ userSchema.methods.passwordChangedAfter = function (JWTTimeStamp) {
   }
 
   return false;
+}
+
+userSchema.methods.createPasswordResetToken = async function () {
+  const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  this.passwordResetExpires = Date.now() + (10 * 60 * 1000) // 10 minutes
+
+  return resetToken;
 }
 
 const User = mongoose.model('User', userSchema);
